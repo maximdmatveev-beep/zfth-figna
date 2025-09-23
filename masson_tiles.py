96128c0509b6cc3032764d62443c6f52c9f366ad
maximdmatveev-beep <maximdmatveev@gmail.com>
2025-09-23 21:35:10 +0300

    Update masson_tiles.py

---
+++ masson_tiles.py
@@ -0,0 +1,22 @@
+while True:
+    number_tiles = input("Enter a tiles number: ")
+    if number_tiles.isdigit():
+        number_tiles = int(number_tiles)
+        if number_tiles % 2 == 0:
+            if number_tiles == 0:
+                print(" ")
+            else:
+                for i in range(number_tiles):
+                    for j in  range(number_tiles):
+                        print('■ □', end=' ')
+                    print()
+        else:
+            print('неверный ввод, введите четное число')
+            continue
+
+    else:
+        print('неверно, введите целое число')
+        continue
+    break
+
+
